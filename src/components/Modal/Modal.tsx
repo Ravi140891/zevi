8f90322be5f6eb24b0fc90c9742522fa505698e4
Ravi Prakash <ravip140891@gmail.com>
2023-04-21 21:28:41 +0530

    Modal design is done

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Modal.scss'
+
+const Modal = () => {
+  return (
+    <div className="modal">
+    <h1>Modal</h1>
+    </div>
+  )
+}
+
+export default Modal
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
 import './SearchBar.scss'
+import Modal from '../Modal/Modal'
 
 const SearchBar = () => {
-    const [searchTerm, setSearchTerm] = useState<string>("")
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [hide, setHide] = useState<boolean>(false)
     const handleSearch = (e:any) => {
         setSearchTerm(e.target.value);
     }
 
+    const handleModal = () =>{
+        setHide(true);
+    }
+
+    const handleModalHide = () => {
+        setHide(false)
+    }
+
   return (
-    <div className="search-wrap">
-        <input type="text" placeholder='Search' value={searchTerm} onChange={handleSearch} />
-        <i className="fa fa-search"></i>
-    </div>
+        <div className="search-wrap">
+            <input type="text" placeholder='Search' value={searchTerm} onChange={handleSearch} onFocus={handleModal} onBlur={handleModalHide} />
+            <i className="fa fa-search"></i>
+            {hide && <Modal/>}
+        </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
