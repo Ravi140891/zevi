75b5378417624bbc3d5ad53770f859fff8bf5c23
Ravi Prakash <ravip140891@gmail.com>
2023-04-21 20:16:04 +0530

    Layout container done

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Layout.scss'
+
+const Layout = () => {
+  return (
+    <div className='layout'>
+        <h2 className='logo'>zevi</h2>
+    </div>
+  )
+}
+
+export default Layout
